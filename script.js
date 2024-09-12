35a258d2d023abf55ed56020dbef8742de4b3433
Csbs department <csbsdatadrive@gmail.com>
2024-09-12 14:31:39 +0530

    CSBS

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('active');
+    });
+
+    $(window).on('scroll load', function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('active');
+
+        if($(window).scrollTop()>60){
+            $('.header').addClass('active');
+        }else{
+            $('.header').removeClass('active');
+        }
+
+        $('section').each(function(){
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        })
+
+    })
+
+})
+
+const colors = ['red', 'blue', 'white'];
+let currentIndex = 0;
+
+function changeColor() {
+    const textElement = document.getElementById('colorText');
+    textElement.style.color = colors[currentIndex];
+    currentIndex = (currentIndex + 1) % colors.length;
+}
+
+setInterval(changeColor, 1000); 
